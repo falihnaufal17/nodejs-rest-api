a3eb110ec23b49c55c340cdce572f2dd4fb9b596
falihnaufal17 <falihnaufal1700@gmail.com>
2022-12-20 15:41:18 +0700

    mv index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const reservationRoute = require('./routes/reservation.route');
+const reservationRoute = require('./src/routes/reservation.route');
 
 app.use(express.json())
 
